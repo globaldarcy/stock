d2b13e348a14e5603e2fc1212ab0b2ff4a95189c
Darcy <289904316@qq.com>
2017-12-14 00:22:23 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentComponent } from './components/content/content.component';
+import { StockManageComponent } from './components/stock-manage/stock-manage.component';
+import { StarsComponent } from './components/stars/stars.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    SidebarComponent,
+    ContentComponent,
+    StockManageComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
